a6ddb700ae45446ba227629f3c4d867e534831ed
trevery <canjia.mail@gmail.com>
2017-11-30 19:09:42 +0800

    add statistic

--- stockFilter.py
+++ stockFilter.py
@@ -36,44 +36,98 @@ def is_r_hammer(code='000927',date='2017-11-28',threshode=0):
         
         if df.open.tolist()[0]-df.low.tolist()[0] <= threshode :
             print('Hit reverse hammer: %s \n' % code)
+            return True
         else:
-            return
+            return False
     else:
-        return
+        return False
 
 
 def is_down_r_hammer(code='000927',
                      date='2017-11-28',
                      threshode=0):
     '''
-    a little different from reverse 
+    
 
-         |     |    <-- high
-         | |   |
-         | |  ---   <-- close
-           |  | |
-              ---   <-- open, low
+                             |     |    <-- high
+                             | |   |
+                             | |  ---   <-- close
+        min(open,close) _____  |  | |
+                                  ---   <-- open, low
 
-    it has no tail, which means : open - low = 0 or little more than 0,like 0.1 
     '''
     df = ts.get_hist_data(code, start='2017-11-27', end=date)
     
-    if df.open.tolist()[0] <= df.close.tolist()[1]:
+    if df.open.tolist()[0] <= min(df.close.tolist()[1], df.open.tolist()[1]):
         if df.open.tolist()[0] - df.low.tolist()[0] <= threshode:
             print('Hit dropping down reverse hammer: %s \n' % code )
+            return True
         else:
-            return
+            return False
     else:
-        return
+        return False
 
 
-report = ts.get_report_data(2017,3)
+def up_or_down(code='000927', date='2017-11-29'):
+    df = ts.get_hist_data(code, date, date)
+    if (df.open.tolist()[0] < df.close.tolist()[0]):
+        print('code %s up' % code)
+        return True
+    else:
+        print('code %s down' % code)
+        return False
+    
 
-for index,code in enumerate(report.code.tolist()[10:50]):
+def get_all_code_set(year=2017, quarter=3):
+    report = ts.get_report_data(2017,3)
+    codeSet = set(report.code.tolist()[0:20])
+    return codeSet
+
+    
+
+
+'''
+get all reverse hammer of day 11-28
+'''
+
+date = '2017-11-28'
+hammerCodeSet = set()
+allCodeSet = get_all_code_set()
+
+for index,code in enumerate(allCodeSet):
     #print('--------------')
     try:
-        is_down_r_hammer(str(code))
+        if is_down_r_hammer(str(code)):
+            hammerCodeSet.add(code)
     except:
         pass
     continue
+
+
+'''
+statistic the change of the dropping down hammer code
+'''
+
+date = '2017-11-29'
+upCount = 0
+downCount = 0
+stayCount = 0
+
+for hammerCode in hammerCodeSet:
+    tag = up_or_down(hammerCode)
+    if tag:
+        upCount += 1
+    elif not(tag):
+        downCount += 1
+    else:
+        stayCount += 1
+
+print('------statistics-----')
+print('totoal hammer: %d' % len(hammerCodeSet))
+print('up numbers: %d' % upCount)
+print('down numbers: %d' % downCount)
+upRate = upCount / len(hammerCodeSet)
+print('up rate: %f' % upRate)
+
+
 
