a6255095e4b2e07e4759c5af3bb91556f37c0ea2
trevery <canjia.mail@gmail.com>
2017-11-30 15:27:28 +0800

    finish is_down_r_hammer function

--- stockFilter.py
+++ stockFilter.py
@@ -13,7 +13,7 @@ today = time.strftime("%Y-%m-%d", time.localtime())
 yesterday = datetime.now() + timedelta(days = -1)
 yesterday = yesterday.strftime("%Y-%m-%d")
 
-def find_r_hammer(code='000927',date='2017-11-28',threshode=0):
+def is_r_hammer(code='000927',date='2017-11-28',threshode=0):
     '''
     check the stock is 'reverse hammer' chart or not
     reverse hammer chart looks like this:
@@ -28,23 +28,48 @@ def find_r_hammer(code='000927',date='2017-11-28',threshode=0):
     '''
     df = ts.get_hist_data(code,start=date,end=date)
     #df0 = ts.get_hist_data(code,yesterday,yesterday)
-    if df.open[0] and df.close[0] and df.high[0] and df.low[0]:
-        if df.open[0]-df.low[0] <= threshode :
-            print('code %s is a reverse hammer chart \n' % code)
+    if df.open.tolist()[0] and df.close.tolist()[0] and df.high.tolist()[0]
+                                                    and df.low.tolist()[0]:
+        if df.open.tolist()[0]-df.low.tolist()[0] <= threshode :
+            print('Hit reverse hammer: %s \n' % code)
         else:
-            pass
+            return
+    else:
+        return
 
+
+def is_down_r_hammer(code='000927',
+                     date='2017-11-28',
+                     threshode=0):
+    '''
+    a little different from reverse 
+
+         |     |    <-- high
+         | |   |
+         | |  ---   <-- close
+           |  | |
+              ---   <-- open, low
+
+    it has no tail, which means : open - low = 0 or little more than 0,like 0.1 
+    '''
+    df = ts.get_hist_data(code, start='2017-11-27', end=date)
+    
+    if df.open.tolist()[0] <= df.close.tolist()[1]:
+        if df.open.tolist()[0] - df.low.tolist()[0] <= threshode:
+            print('Hit dropping down reverse hammer: %s \n' % code )
+        else:
+            return
+    else:
+        return
 
 
 report = ts.get_report_data(2017,3)
 
-for index,code in enumerate(report.code):
+for index,code in enumerate(report.code.tolist()[10:50]):
+    #print('--------------')
     try:
-        find_r_hammer(str(code),'2017-11-29')
+        is_down_r_hammer(str(code))
     except:
-        '''
-        ignore the except and continue
-        '''
         pass
     continue
 
